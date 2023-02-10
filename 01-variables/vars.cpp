1f9fa47f9b983c913a588c1f4ef516d4cfb4507e
iluvatar1 <woquendo@gmail.com>
2023-02-10 10:39:31 -0500

    Ejemplo de variables, declaracion, inicializacion

---
+++ 01-variables/vars.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+  int i = 2000000000;
+  int j = 3000000000;
+  bool flag = false;
+  float x = 0.9877, y= -0.987;
+  double z = 1.23e10, w = -1.8765e-20;
+
+  std::cout << "i = " << i << "\n";
+  std::cout << "j = " << j << "\n";
+  std::cout << "flag = " << flag << "\n";
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n";
+  std::cout << "z = " << z << "\n";
+  std::cout << "w = " << w << "\n";
+
+  return 0;
+}
