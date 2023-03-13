43f88489467e015dd675eab19f03a0a397de3cd0
iluvatar1 <woquendo@gmail.com>
2023-03-13 17:13:46 -0500

    Added examples for derivatives

---
+++ 06-function_templates/deriv_float.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+using fptr = double(double);
+
+float g(float x);
+double f(double x);
+
+template <typename T>
+double deriv(T fun, double x, double h);
+
+int main(int argc , char **argv)
+{
+  std::cout.setf(std::ios::scientific);
+  std::cout.precision(15);
+
+  double x{2.3}, h{0.001};
+
+  std::cout << deriv(f, x, h) << "\n";
+  std::cout << deriv(g, x, h) << "\n";
+
+  return 0;
+}
+
+double f(double x)
+{
+  return 2*std::sin(x) - x;
+}
+
+float f(float x)
+{
+  return 2*std::cos(x) - x;
+}
+
+template <typename T>
+double deriv(T fun, double x, double h)
+{
+  return (fun(x+h) - fun(x-h))/(2*h);
+}
+
---
+++ 06-function_templates/deriv_template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using fptr = double(double);
+
+double f(double x);
+double deriv(fptr fun, double x, double h);
+
+int main(int argc , char **argv)
+{
+  std::cout.setf(std::ios::scientific);
+  std::cout.precision(15);
+
+  double x{2.3}, h{0.001};
+
+  std::cout << deriv(f, x, h) << "\n";
+
+  return 0;
+}
+
+double f(double x)
+{
+  return 2*std::sin(x) - x;
+}
+
+double deriv(fptr fun, double x, double h)
+{
+  return (fun(x+h) - fun(x-h))/(2*h);
+}
+
